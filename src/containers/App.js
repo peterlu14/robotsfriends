64f22044cab605c95d977fa1fbb1c0440ed990a5
peterlu14 <peterlu810516@gmail.com>
2019-04-27 01:29:34 +0800

    add redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,48 @@
 import React , {PureComponent} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList'; 
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
-import {robots} from '../Robots.js';
+//import {robots} from '../Robots.js';
 import './App.css';
 
-class App extends PureComponent {
-  constructor(){
-    super()
-    this.state = {
-      robots:[],
-      searchfield:''
-    }
+import  { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+//@connect(mapStateToProps, mapDispatchToProps)
+class App extends PureComponent {
 
   componentDidMount(){
-    //fetch way
-    /*fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({robots:users}));*/
-    //file way
-    this.setState({robots:robots})
-  }
-  
-  onSearchChange=(event) => {
-    this.setState({searchfield:event.target.value});
+   this.props.onRequestRobots();
   }
 
   render(){
-    const {robots,searchfield} = this.state;
+    const { searchField, onSearchChange, robots, isPending} = this.props;
     const filterRobots = (robots.filter(robot=> {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     }))
-    return !robots.length ?
+    return isPending ?
     <h1>Loading</h1> :
     (
       <div className='tc'>
       <h1 className='f1'>robotfriend</h1>
-      <SearchBox searchChange={this.onSearchChange}/>
+      <SearchBox searchChange={onSearchChange}/>
       <Scroll>
         <ErrorBoundry>
           <CardList robots={filterRobots}/>
@@ -49,4 +53,4 @@ class App extends PureComponent {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
